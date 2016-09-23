651bd6fa82dd30b31830e4b6cf0e22f377cf8d07
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-09-23 16:42:17 -0600

    Adding Code Stub and Test projects

---
+++ PageObjects/SearchPage.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjects
+{
+    public class SearchPage
+    {
+        IWebDriver driver;
+
+        #region Constuctors
+        public SearchPage(IWebDriver Driver)
+        {
+            //This constructor needs to initialize the WebElements that are listed in the "Page Elements" region.
+            //If using PageFactory, just use the PageFactory.initElements(WebDriver, pageObjects.SearchPage) method.
+        }
+        #endregion
+
+        #region Page Elements
+
+        // USE THE [FindsBy] NOTATION OF THE PAGEFACTORY CLASS
+        //    TO LOCATE AND IDENTIFY THESE WEB PAGE ELEMENTS:
+
+        [FindsBy(How = How.Name, Using = "filter-category-select")]
+        public IWebElement whatSelect { get; set; }
+
+        
+            public IWebElement subCategorySelect;
+
+            public IWebElement byResortSelect;
+
+            public IWebElement okSubmitButton;
+
+            List<IWebElement> searchResults;
+        
+
+        #endregion
+
+        #region Methods
+
+        public List<String> SearchFor(String what, String subCategory, String byResort)
+            {
+                List<String> results = new List<String>();
+                results.Add("This just initializes the list so the code will compile. Delete this entire line of code.");
+                //TODO Enter the parameters into the correct page elements.
+                //TODO Click the 'OK" button.
+                //TODO Get a string representing each result from the ListingResults-item section of the web page.This can be the 'title,' the 'copy,' or whatever.
+                //TODO Add each String to the list of results.
+                //TODO return the results.
+                return results;
+            }
+        #endregion
+    }
+
+}
---
+++ VerificationTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using PageObjects;
+
+[TestClass]
+public class VerificationTests {
+    public IWebDriver driver;
+    ChromeDriverService service;
+    ChromeOptions opts;
+    TimeSpan thirtySeconds;
+    SearchPage searchPage;
+
+    [AssemblyInitialize]
+    public void Pretest()
+    {
+        service = ChromeDriverService.CreateDefaultService();
+        opts = new ChromeOptions();
+        thirtySeconds = new TimeSpan(0, 0, 30);
+        driver = new ChromeDriver(service, opts, thirtySeconds);
+        try
+        {
+            driver.Url= "https://www.skiutah.com/members/listing";
+            searchPage = new SearchPage(driver);
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.Message);
+        }
+    }
+
+    #region Tests
+    [TestMethod]
+    public void verifyWhatSelectIsFound()
+    {
+        Assert.IsNotNull(searchPage.whatSelect);
+    }
+
+    [TestMethod]
+    public void verifySubCategorySelectIsFound()
+    {
+        Assert.IsNotNull(searchPage.subCategorySelect);
+    }
+
+    [TestMethod]
+    public void verifyByResortSelectIsFound()
+    {
+        Assert.IsNotNull(searchPage.byResortSelect);
+    }
+
+    [TestMethod]
+    public void verifyOkSubmitButtonFound()
+    {
+        Assert.IsNotNull(searchPage.okSubmitButton);
+    }
+
+    [TestMethod]
+    public void verifySearchResults1()
+    {
+        Assert.IsFalse(searchPage.SearchFor("Activities", "", "").Count>0);
+    }
+
+   
+    [TestMethod]
+    public void verifySearchResults2()
+    {
+        Assert.IsFalse(searchPage.SearchFor("Ski School", "", "").Count > 0);
+    }
+
+   
+    [TestMethod]
+    public void verifySearchResults3()
+    {
+        Assert.IsFalse(searchPage.SearchFor("Ski School", "Clinics", "Sundance").Count > 0);
+    }
+    #endregion
+
+    [AssemblyCleanup]
+    public void TearDown()
+    {
+        driver.Close();
+    }
+}
